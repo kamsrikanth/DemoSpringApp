eba9d03bd3ca6f69031b8a74783fc5773e85155b
Ashwath Venkataraman <42375646+ashwath129@users.noreply.github.com>
2021-07-19 16:33:33 -0400

    Update User.java

--- src/main/java/com/journaldev/spring/model/User.java
+++ src/main/java/com/journaldev/spring/model/User.java
@@ -2,6 +2,7 @@ package com.journaldev.spring.model;
 
 public class User {
 	private String userName;
+	private String num;
 
 	public String getUserName() {
 		return userName;
@@ -9,5 +10,13 @@ public class User {
 
 	public void setUserName(String userName) {
 		this.userName = userName;
+	}
+	
+	public String getNum() {
+		return num;
+	}
+
+	public void setNum(String num) {
+		this.num = num;
 	}
 }
