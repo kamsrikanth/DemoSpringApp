138529b2b55731bf6fdf82071a73e5e74058c29b
Ashwath Venkataraman <42375646+ashwath129@users.noreply.github.com>
2021-07-19 16:50:50 -0400

    Update HomeController.java

--- src/main/java/com/journaldev/spring/controller/HomeController.java
+++ src/main/java/com/journaldev/spring/controller/HomeController.java
@@ -3,17 +3,23 @@ package com.journaldev.spring.controller;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.apache.log4j.Logger;
 
 import com.journaldev.spring.model.User;
 
 @Controller
 public class HomeController {
+	private static final Logger log = Logger.getLogger(HomeController.class.getName());
 
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -34,6 +40,19 @@ public class HomeController {
 	@RequestMapping(value = "/user", method = RequestMethod.POST)
 	public String user(@Validated User user, Model model) {
 		System.out.println("User Page Requested");
+		String numerator = user.getNum();
+		int numeratorInt = Integer.parseInt(numerator);
+		try {
+			int quotient = numeratorInt/0;	
+		}
+		catch (Exception e) {
+            	   log.error(e.getMessage());
+            	   StringWriter writer = new StringWriter();
+                   PrintWriter pw = new PrintWriter(writer);
+                   e.printStackTrace(pw);
+            	   log.error(writer.toString());
+               }
+		
 		model.addAttribute("userName", user.getUserName());
 		return "user";
 	}
